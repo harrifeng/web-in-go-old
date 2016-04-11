091ec9dc229aa2188bf7a7afb73ff2b36d5393d3
Haoran Feng <haoran.feng@sap.com>
2016-04-11 16:48:59 +0800

    finish CURD

--- main.go
+++ main.go
@@ -59,10 +59,46 @@ func GetNoteHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write(j)
 }
 
+func PutNoteHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	k := vars["id"]
+	var noteToUpd Note
+
+	err := json.NewDecoder(r.Body).Decode(&noteToUpd)
+
+	if err != nil {
+		panic(err)
+	}
+
+	if note, ok := noteStore[k]; ok {
+		noteToUpd.CreatedOn = note.CreatedOn
+		delete(noteStore, k)
+		noteStore[k] = noteToUpd
+	} else {
+		log.Printf("Cound not find key of Note %s to delete", k)
+	}
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func DeleteNoteHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	k := vars["id"]
+
+	if _, ok := noteStore[k]; ok {
+		delete(noteStore, k)
+	} else {
+		log.Printf("Cound not find key of Note %s to delete", k)
+	}
+	w.WriteHeader(http.StatusNoContent)
+}
+
 func main() {
 	r := mux.NewRouter().StrictSlash(false)
 	r.HandleFunc("/api/notes", GetNoteHandler).Methods("GET")
 	r.HandleFunc("/api/notes", PostNoteHandler).Methods("POST")
+	r.HandleFunc("/api/notes/{id}", PutNoteHandler).Methods("PUT")
+	r.HandleFunc("/api/notes/{id}", DeleteNoteHandler).Methods("DELETE")
 
 	server := &http.Server{
 		Addr:    ":8080",
