711bcef70db6039209b1444e5261eee2ef086b36
Haoran Feng <haoran.feng@sap.com>
2016-04-11 16:31:55 +0800

    http request body is actually a string, after the Read(), the body will have nothing inside

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"strconv"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -23,25 +22,38 @@ var noteStore = make(map[string]Note)
 var id int = 0
 
 func PostNoteHandler(w http.ResponseWriter, r *http.Request) {
-	var note Note
+	// var note Note
 
-	err := json.NewDecoder(r.Body).Decode(&note)
-	if err != nil {
-		panic(err)
+	bufferSize := 100
+
+	p := make([]byte, bufferSize)
+	len, err2 := r.Body.Read(p)
+	fmt.Println(len)
+	if err2 != nil {
+		fmt.Println("error is ", err2.Error())
 	}
 
-	note.CreatedOn = time.Now()
-	id++
-	k := strconv.Itoa(id)
-	noteStore[k] = note
-
-	j, err := json.Marshal(note)
-	if err != nil {
-		panic(err)
+	for i := 0; i < len; i++ {
+		fmt.Printf("%c", p[i])
 	}
+
+	// err := json.NewDecoder(r.Body).Decode(&note)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	//
+	// note.CreatedOn = time.Now()
+	// id++
+	// k := strconv.Itoa(id)
+	// noteStore[k] = note
+	//
+	// j, err := json.Marshal(note)
+	// if err != nil {
+	// 	panic(err)
+	// }
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
-	w.Write(j)
+	// w.Write(j)
 }
 
 func GetNoteHandler(w http.ResponseWriter, r *http.Request) {
